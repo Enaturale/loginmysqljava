4f0396294657cf571d1056d5305b8f3b4e0e1457
The Real Tech Lady <bukolaarowosebi@gmail.com>
2022-04-21 17:34:38 +0100

    Login form with mysql database

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,13 +5,15 @@ public class MainFrame extends JFrame implements ActionListener{
 
     ImageIcon images[];
     JButton next, prev;
+    JLabel imageLabel;
+    int i,l1;
 
     public void initialize(User user){
        
 
         JPanel infoPanel = new JPanel();
-        infoPanel.setLayout(new GridLayout(0, 2, 5, 5));
-        infoPanel.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
+        infoPanel.setLayout(new GridLayout(0, 2, 2, 2));
+        infoPanel.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 20));
 
         infoPanel.add(new JLabel("Name"));
         infoPanel.add(new JLabel(user.name));
@@ -32,13 +34,21 @@ public class MainFrame extends JFrame implements ActionListener{
         add(infoPanel, BorderLayout.NORTH);
 
         JPanel sliderPanel = new JPanel();
+        images = new ImageIcon[4];
+        images[0] = new ImageIcon("C:/Users/User/Documents/portfolio/java/loginmysql/src/saisho.jpg");
+        images[1] = new ImageIcon("C:/Users/User/Documents/portfolio/java/loginmysql/src/blender.jpg");
+        images[2] = new ImageIcon("C:/Users/User/Documents/portfolio/java/loginmysql/src/t55.jpg");
+        images[3] = new ImageIcon("C:/Users/User/Documents/portfolio/java/loginmysql/src/watch.jpg");
 
+        imageLabel = new JLabel(" ", JLabel.CENTER);
+        imageLabel.setIcon(images[0]);
 
         add(sliderPanel, BorderLayout.CENTER);
+        sliderPanel.add(imageLabel);
 
         JPanel buttonPanel = new JPanel(new FlowLayout());
-        next = new JButton(">>");
-        prev = new JButton("<<");
+        next = new JButton("<<");
+        prev = new JButton(">>");
         buttonPanel.add(next);
         buttonPanel.add(prev);
         next.addActionListener(this);
@@ -58,7 +68,29 @@ public class MainFrame extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
+
+        if(e.getSource() == next){
+            if(i == 0){
+                
+            }else{
+                i = i - 1;
+                imageLabel.setIcon(images[i]);
+            }
+        }
+
+        if(e.getSource() == prev){
+            if(i==images.length-1)
+        {
+            //JOptionPane.showMessageDialog(null,"This is LastImage");
+        }
+        else
+            {
+            i=i+1;
+            imageLabel.setIcon(images[i]);
+            }
+        }
+        }
         
     }
 
-}
+
